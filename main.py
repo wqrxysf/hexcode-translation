3bbf077249ba4119567703f1829db95797c3d438
wqrxysf <wqrxysf@yandex.ru>
2023-09-29 20:32:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+masks = {
+
+}
+
+
+def swipe_bit():
+    pass
+
+
+def find_mask():
+    pass
+
+
+def j_c_commands():
+    pass
+
+
+def ld_su_commands():
+    pass
+
+
+def rj_breq_bren_commands():
+    pass
+
+
+def other_commands():
+    pass
+
+
+def from_hex_to_bin():
+    pass
+
+
+def from_bin_to_oct():
+    pass
+
+
+if __name__ == '__main__':
+    pass
